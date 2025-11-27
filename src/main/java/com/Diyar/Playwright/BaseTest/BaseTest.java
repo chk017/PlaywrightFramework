bc60c9912a29500b69dda911594e37bf074cfed2
Chennakesava Kaja <chk017@diyarme.com>
2025-11-27 11:58:42 +0530

    change in pom

--- src/main/java/com/Diyar/Playwright/BaseTest/BaseTest.java
+++ src/main/java/com/Diyar/Playwright/BaseTest/BaseTest.java
@@ -35,7 +35,7 @@ import java.util.HashMap;
 /**
  * BaseTest class
  * @author ChennakesavaRao Bachu
- * 07-Nov-2025
+ * 27-Nov-2025
  */
 public class BaseTest {
 	protected static String sProjectDirectory = System.getProperty("user.dir");
